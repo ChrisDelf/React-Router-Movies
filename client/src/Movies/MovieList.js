26a05eacaf32a07c76cf4eb93c2918818c152131
Chris-Delfaus <ChrisDelf@protonmail.com>
2019-07-22 17:36:35 -0400

    stretch refactored code

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import {Link} from 'react-router-dom';
+import MovieCard from './MovieCard';
 const MovieList = props => {
   const [movies, setMovies] = useState([])
-console.log (props)
+
   useEffect(() => {
     const getMovies = () => {
       axios
@@ -29,27 +30,9 @@ console.log (props)
 }
 
 function MovieDetails({ movie }) {
-  const { title, director, metascore, stars } = movie;
+console.log('MovieDetailes', movie)
   return (
-    <Link to=  '/movies/:id' >
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
-
-      {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
-        </div>
-
-      ))}
-    </div>
-    </Link>
+    <MovieCard key = {movie.id} movie ={movie}/>
   );
 }
 
