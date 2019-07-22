6d83b3583e023ef35eee5320a11cf218b5d0cefe
Chris-Delfaus <ChrisDelf@protonmail.com>
2019-07-22 16:46:00 -0400

    thanks to help was about to get is semi working

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import {Link} from 'react-router-dom';
 const MovieList = props => {
   const [movies, setMovies] = useState([])
+console.log (props)
   useEffect(() => {
     const getMovies = () => {
       axios
@@ -14,13 +15,13 @@ const MovieList = props => {
           console.error('Server Error', error);
         });
     }
-    
+
     getMovies();
   }, []);
-  
+
   return (
     <div className="movie-list">
-      {this.state.movies.map(movie => (
+      {movies.map(movie => (
         <MovieDetails key={movie.id} movie={movie} />
       ))}
     </div>
@@ -30,7 +31,7 @@ const MovieList = props => {
 function MovieDetails({ movie }) {
   const { title, director, metascore, stars } = movie;
   return (
-    <div className="movie-card">
+    <Link to=  '/movies/:id' className="movie-card">
       <h2>{title}</h2>
       <div className="movie-director">
         Director: <em>{director}</em>
@@ -45,7 +46,7 @@ function MovieDetails({ movie }) {
           {star}
         </div>
       ))}
-    </div>
+    </Link>
   );
 }
 
